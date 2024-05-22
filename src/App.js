1bffbad343bd7de466a74b348974fdc2fadded14
ball-hand <ikbalhand13@gmail.com>
2024-05-22 20:02:08 +0700

    Two

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing react frist time 
         </a>
       </header>
     </div>
